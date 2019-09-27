fe1c9782ad0a35c5d9c774b02dc25486d0a038b0
BlackOfWorld <16164933+BlackOfWorld@users.noreply.github.com>
2019-09-27 10:41:59 +0200

    there's no 1/10% chance to teleport player back in fakelag fakelag now has block placement too
    Signed-off-by: BlackOfWorld <16164933+BlackOfWorld@users.noreply.github.com>

--- src/main/java/me/bow/treecapitatorultimate/commands/Player/LagPlayer.java
+++ src/main/java/me/bow/treecapitatorultimate/commands/Player/LagPlayer.java
@@ -3,15 +3,17 @@ package me.bow.treecapitatorultimate.commands.Player;
 import me.bow.treecapitatorultimate.Start;
 import me.bow.treecapitatorultimate.command.Command;
 import me.bow.treecapitatorultimate.command.CommandCategory;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 import java.util.UUID;
 
@@ -26,7 +28,7 @@ public class LagPlayer extends Command {
     @Override
     public void onPlayerBlockBreak(BlockBreakEvent e) {
         Player p = e.getPlayer();
-        if (players.contains(p.getUniqueId())) return;
+        if (!players.contains(p.getUniqueId())) return;
         ItemStack item = p.getInventory().getItemInMainHand();
         if (p.getGameMode() == GameMode.CREATIVE && p.getInventory().getItemInMainHand().getType().toString().toLowerCase().contains("sword"))
             return;
@@ -36,6 +38,31 @@ public class LagPlayer extends Command {
             Bukkit.getScheduler().runTaskLater(Start.Instance, () -> e.getBlock().breakNaturally(item), rnd.nextInt((25 - 10) + 1) + 10);
         }
         e.setCancelled(true);
+    }
+
+    @Override
+    public void onPlayerBlockPlace(BlockPlaceEvent e) {
+        Player p = e.getPlayer();
+        if (!players.contains(p.getUniqueId())) return;
+        BlockData block = e.getBlock().getBlockData();
+        ItemStack blockPlace = e.getItemInHand();
+        Runnable runnable = () -> e.getBlockPlaced().setBlockData(block);
+        if (p.getGameMode() == GameMode.CREATIVE) {
+            Bukkit.getScheduler().runTaskLater(Start.Instance, runnable, rnd.nextInt((25 - 10) + 1) + 10);
+        } else {
+            Bukkit.getScheduler().runTaskLater(Start.Instance, runnable, rnd.nextInt((25 - 10) + 1) + 10);
+        }
+        e.setCancelled(true);
+    }
+
+    @Override
+    public void onPlayerMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        if (!players.contains(p.getUniqueId())) return;
+        if (rnd.nextInt(100) > 10) return;
+        Location from = e.getFrom();
+        if (from.getBlock().equals(Objects.requireNonNull(e.getTo()).getBlock())) return;
+        Bukkit.getScheduler().runTaskLater(Start.Instance, () -> p.teleport(from), 10);
     }
 
     @Override
